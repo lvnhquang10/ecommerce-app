35cb6490c9b79888be40038ef68bed312efa23f8
lvnhquang10 <lv.nhquang10@gmail.com>
2023-06-20 15:15:09 +0700

    Changing html code in login component

--- src/app/component/customer-auth/customer-auth.component.ts
+++ src/app/component/customer-auth/customer-auth.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-customer-auth',
   templateUrl: './customer-auth.component.html',
   styleUrls: ['./customer-auth.component.css']
 })
-export class CustomerAuthComponent {
+export class CustomerAuthComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+  SignupComponent(data:object): void {
+    console.warn(data)
+  }
+  
 
 }
